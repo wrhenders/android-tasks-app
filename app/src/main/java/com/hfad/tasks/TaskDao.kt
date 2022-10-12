4f2fc8dbd1c656d5428a1ad67c6225adcae813e6
Ryan Henderson <wrhenders@gmail.com>
2022-10-12 13:31:02 -0500

    added ViewModel and factory and linked to fragment and layout

--- app/src/main/java/com/hfad/tasks/TaskDao.kt
+++ app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -11,13 +11,13 @@ import androidx.room.Update
 interface TaskDao {
 
     @Insert
-    fun insert(task: Task)
+    suspend fun insert(task: Task)
 
     @Update
-    fun update(task: Task)
+    suspend fun update(task: Task)
 
     @Delete
-    fun delete(task: Task)
+    suspend fun delete(task: Task)
 
     @Query("SELECT * FROM task_table WHERE taskId = :taskId")
     fun get(taskId: Long): LiveData<Task>
--- app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.hfad.tasks.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
@@ -17,6 +18,15 @@ class TasksFragment : Fragment() {
     ): View? {
         _binding = FragmentTasksBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+        val viewModelFactory = TasksViewModelFactory(dao)
+        val viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(TasksViewModel::class.java)
+
+        binding.viewModel = viewModel
+
         return view
     }
 
