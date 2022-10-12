a61e532364618bfd36f8c67ee9e189269c8376d0
Ryan Henderson <wrhenders@gmail.com>
2022-10-12 14:04:55 -0500

    added getAll and display tasks in db in fragment layout

--- app/src/main/java/com/hfad/tasks/TasksFragment.kt
+++ app/src/main/java/com/hfad/tasks/TasksFragment.kt
@@ -26,6 +26,7 @@ class TasksFragment : Fragment() {
             .get(TasksViewModel::class.java)
 
         binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
 
         return view
     }
