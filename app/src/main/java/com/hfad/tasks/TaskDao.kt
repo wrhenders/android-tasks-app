648542b4095f2fbfcf4a6dd5ddc8fd8396138f66
Ryan Henderson <wrhenders@gmail.com>
2022-10-12 13:16:56 -0500

    added database and Dao

--- app/src/main/java/com/hfad/tasks/TaskDao.kt
+++ app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -1,7 +1,10 @@
 package com.hfad.tasks
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 
 @Dao
@@ -12,4 +15,14 @@ interface TaskDao {
 
     @Update
     fun update(task: Task)
+
+    @Delete
+    fun delete(task: Task)
+
+    @Query("SELECT * FROM task_table WHERE taskId = :taskId")
+    fun get(taskId: Long): LiveData<Task>
+
+    @Query("SELECT * FROM task_table ORDER BY taskId DESC")
+    fun getAll(): LiveData<List<Task>>
+
 }
