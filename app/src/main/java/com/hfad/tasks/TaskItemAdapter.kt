91788a1c8d9e2ec99c1e7ccc3949bc5453593704
Ryan Henderson <wrhenders@gmail.com>
2022-10-13 11:22:18 -0500

    updated layout for grid view

--- app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
+++ app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
@@ -2,7 +2,9 @@ package com.hfad.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class TaskItemAdapter: RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>() {
@@ -12,6 +14,8 @@ class TaskItemAdapter: RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>(
             notifyDataSetChanged()
         }
 
+    override fun getItemCount() = data.size
+
     override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
         val item = data[position]
         holder.bind(item)
@@ -19,19 +23,22 @@ class TaskItemAdapter: RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
             TaskItemViewHolder = TaskItemViewHolder.inflateFrom(parent)
-    override fun getItemCount() = data.size
 
-    class TaskItemViewHolder(val rootView: TextView):RecyclerView.ViewHolder(rootView) {
+    class TaskItemViewHolder(val rootView: CardView):RecyclerView.ViewHolder(rootView) {
+        val taskName = rootView.findViewById<TextView>(R.id.task_name)
+        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as TextView
+                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as CardView
 
                 return TaskItemViewHolder(view)
             }
         }
         fun bind(item:Task) {
-            rootView.text = item.taskName
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
         }
     }
 }
