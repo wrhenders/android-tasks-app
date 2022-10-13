3e30c5ec2262e7d02bb87925aa5684c125e2688a
Ryan Henderson <wrhenders@gmail.com>
2022-10-13 13:06:52 -0500

    added editViewModel and calls to update or delete from Room database

--- app/src/main/java/com/hfad/tasks/EditTaskFragment.kt
+++ app/src/main/java/com/hfad/tasks/EditTaskFragment.kt
@@ -6,18 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.hfad.tasks.databinding.FragmentEditTaskBinding
+import com.hfad.tasks.databinding.FragmentTasksBinding
 
 class EditTaskFragment : Fragment() {
+    private var _binding : FragmentEditTaskBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_edit_task, container, false)
-        val textView = view.findViewById<TextView>(R.id.task_id)
+        _binding = FragmentEditTaskBinding.inflate(inflater, container, false)
+        val view = binding.root
         val taskId = EditTaskFragmentArgs.fromBundle(requireArguments()).taskId
-        textView.text = taskId.toString()
+        val application = requireNotNull(this.activity).application
+        val dao = TaskDatabase.getInstance(application).taskDao
+
+        val viewModelFactory = EditTaskViewModelFactory(taskId, dao)
+        val viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(EditTaskViewModel::class.java)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        viewModel.navigateToList.observe(viewLifecycleOwner, Observer { navigate ->
+            if(navigate) {
+                view.findNavController().navigate(R.id.action_editTaskFragment_to_tasksFragment)
+                viewModel.onNavigatedToList()
+            }
+        })
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
